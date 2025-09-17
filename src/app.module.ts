9fedbe5e4e01997c13310017190eb84bb59ad7bf
Danish Joseph <danishjoseph07@gmail.com>
2025-09-17 18:46:18 +0200

    feat: add typeorm initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import datasource from './typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(datasource.options),
   ],
   controllers: [],
   providers: [],
---
+++ src/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+config();
+const configService = new ConfigService();
+
+const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.getOrThrow<string>('DB_HOST'),
+  port: configService.getOrThrow<number>('DB_PORT'),
+  username: configService.getOrThrow<string>('DB_USERNAME'),
+  password: configService.getOrThrow<string>('DB_PASSWORD'),
+  database: configService.getOrThrow<string>('DB_NAME'),
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/../migrations/*.{js,ts}'],
+  migrationsRun: false,
+  synchronize: false,
+  logging: process.env.ENV !== 'production',
+};
+
+export default new DataSource(dataSourceOptions);
