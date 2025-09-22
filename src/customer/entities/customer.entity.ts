c3d40fa6836fd92b4f3b6282638ee8aba06e021b
Danish Joseph <danishjoseph07@gmail.com>
2025-09-22 07:03:48 +0200

    feat: add product entity typeorm gql schema

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -5,8 +5,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   Index,
+  OneToMany,
 } from 'typeorm';
 import { ObjectType, Field } from '@nestjs/graphql';
+import { Product } from '../../product/entities/product.entity';
 
 @Entity('customers')
 @ObjectType()
@@ -68,4 +70,8 @@ export class Customer {
   @UpdateDateColumn()
   @Field()
   updated_at: Date;
+
+  @OneToMany(() => Product, (product) => product.customer, { cascade: true })
+  @Field(() => [Product])
+  products: Product[];
 }
---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,67 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import { registerEnumType } from '@nestjs/graphql';
+import { ProductCategory } from '../enums/product.category.enum';
+import { Customer } from '../../customer/entities/customer.entity';
+
+registerEnumType(ProductCategory, {
+  name: 'ProductCategory',
+});
+
+@Entity('products')
+@ObjectType()
+@Unique(['serial_number'])
+@Index('IDX_PRODUCT_SERIAL_NUMBER', ['serial_number'])
+export class Product {
+  @PrimaryGeneratedColumn()
+  @Field()
+  id: number;
+
+  @Column({ length: 100, unique: true })
+  @Field()
+  serial_number: string;
+
+  @Column({
+    type: 'enum',
+    enum: ProductCategory,
+  })
+  @Field(() => ProductCategory)
+  category: ProductCategory;
+
+  @Column({ length: 100 })
+  @Field()
+  name: string;
+
+  @Column({ length: 100, nullable: true })
+  @Field({ nullable: true })
+  brand: string;
+
+  @Column({ length: 100, nullable: true })
+  @Field({ nullable: true })
+  model_name: string;
+
+  @CreateDateColumn()
+  @Field()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  @Field()
+  updated_at: Date;
+
+  @ManyToOne(() => Customer, (customer) => customer.products, {
+    nullable: true,
+  })
+  @JoinColumn({ name: 'customer_id' })
+  @Field(() => Customer)
+  customer: Customer;
+}
