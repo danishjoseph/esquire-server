e0b09f4e11a2510a7ffb31c7e8e91e03ecea770b
Danish Joseph <danishjoseph07@gmail.com>
2025-09-27 04:41:51 +0200

    fixup! feat: remove product customer direct relation

--- migrations/1758859327844-remove-product-customer-relation.ts
+++ migrations/1758859327844-remove-product-customer-relation.ts
@@ -6,16 +6,12 @@ export class RemoveProductCustomerRelation1758859327844
   name = 'RemoveProductCustomerRelation1758859327844';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "products" DROP CONSTRAINT "FK_91c1e7c771331a85a9a4c3f638e"`,
-    );
+    await queryRunner.query( `ALTER TABLE "products" DROP CONSTRAINT "FK_91c1e7c771331a85a9a4c3f638e"`,);
     await queryRunner.query(`ALTER TABLE "products" DROP COLUMN "customer_id"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "products" ADD "customer_id" integer`);
-    await queryRunner.query(
-      `ALTER TABLE "products" ADD CONSTRAINT "FK_91c1e7c771331a85a9a4c3f638e" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
+    await queryRunner.query( `ALTER TABLE "products" ADD CONSTRAINT "FK_91c1e7c771331a85a9a4c3f638e" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,);
   }
 }
