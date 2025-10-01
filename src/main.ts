fde918e2a30a74cf6e1b8038cdbdd97b77a89dd7
Danish Joseph <danishjoseph07@gmail.com>
2025-10-01 21:41:36 +0200

    feat: add class validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      errorHttpStatusCode: 422,
+      exceptionFactory: (errors) => new BadRequestException(errors),
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
